adf287a9850e7c5c0f270bba7fd013b6f9a956f3
Tolala9 <anatolii.lukavenko@gmail.com>
2018-03-02 00:03:02 +0200

    Navigation menu and pages

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php 
+
+function WPThemeDevPrac_resources(){
+
+	wp_enqueue_style('style', get_stylesheet_uri());
+
+}
+add_action('wp_enqueue_scripts', 'WPThemeDevPrac_resources');
+
+
+
+// Navigation Menus
+register_nav_menus(array(
+	'primary' => __('Primary Menu'),
+	'footer' => __('Footer Menu'),
+
+));
+
+
+
+
+ ?>
