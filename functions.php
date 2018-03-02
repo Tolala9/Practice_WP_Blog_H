89a1f85be15e2af41089edc863467784a0baa64d
Tolala9 <anatolii.lukavenko@gmail.com>
2018-03-02 23:04:44 +0200

    Add child page menu

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php 
 
+// Connect style.css to header.php
 function WPThemeDevPrac_resources(){
 
 	wp_enqueue_style('style', get_stylesheet_uri());
@@ -15,6 +16,31 @@ register_nav_menus(array(
 	'footer' => __('Footer Menu'),
 
 ));
+
+// Get top ancestor
+function get_top_ancestor_id() {
+
+	global $post;
+
+	if ($post->post_parent) {
+		
+		$ancestors = array_reverse(get_post_ancestors($post->ID));
+		return $ancestors[0];
+	}
+
+	return  $post->ID; 
+}
+
+// Does page have children?
+function has_shidlren() {
+
+	global $post;
+
+	$pages = get_pages('child_of=' . $post->ID);
+	return count($pages);
+
+}
+
 
 
 
--- page-first-page.php
+++ page-first-page.php
@@ -6,6 +6,36 @@
 		while (have_posts()) : the_post(); ?>
 
 			<article class="post page">
+			
+								<!-- Sub Menu -->
+				<?php  
+				if (has_shidlren() OR $post->post_parent > 0) { ?>
+
+
+				<nav class="site-nav children-links clearfix">
+
+					<span class="parent-link">
+
+						<a href="<?php echo get_the_permalink(get_top_ancestor_id()); ?>">
+						<?php echo get_the_title(get_top_ancestor_id()); ?>
+							
+						</a></span> 
+
+						<ul>
+							<?php  
+							$args = array(
+								'child_of' => get_top_ancestor_id(),
+								'title_li' => ''
+							)
+							?>
+
+							<?php wp_list_pages($args); ?>
+						</ul>
+
+					</nav>
+
+				<?php } ?>
+				<!-- /Sub Menu -->
 
 				<!-- column container -->
 				<div class="column-container clearfix">
