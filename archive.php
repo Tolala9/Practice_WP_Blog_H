2b6742e3f35f6792e0371fbb5636ffd28c54e493
Tolala9 <anatolii.lukavenko@gmail.com>
2018-03-03 23:34:17 +0200

    Archive.php & Excerpts for posts

--- archive.php
+++ archive.php
@@ -3,6 +3,33 @@
 	get_header();
 
 	if (have_posts()) :
+		// For archive
+		?>
+
+		<h2><?php 
+
+			if (is_category()) {
+				single_cat_title();
+			} elseif (is_tag()) {
+				single_tag_title();
+			} elseif (is_author()) {
+				the_post();
+				echo 'Author Archives: ' . get_the_author();
+				rewind_posts();
+			} elseif (is_day()) {
+				echo 'Daily Archives: ' . get_the_date();
+			} elseif (is_month()) {
+				echo 'Monthly Archives: ' . get_the_date('F Y');
+			} elseif (is_year()) {
+				echo 'Monthly Archives: ' . get_the_date('Y');
+			} else {
+				echo 'Archives:';
+			}
+
+		 ?></h2>
+
+		<?php
+
 		while (have_posts()) : the_post(); ?>
 
 			<article class="post">
@@ -25,7 +52,7 @@
 				 ?>
 			</p>
 			<!-- /Post info -->
-			<?php the_content('Continue reading &raquo;') ?>
+			<?php the_excerpt() ?> <!-- change for content display -->
 
 			</article>
 
@@ -41,3 +68,4 @@
 	get_footer();
 
  ?>
+
